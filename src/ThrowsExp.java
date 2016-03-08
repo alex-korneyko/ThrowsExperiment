4bfb945b40428646fe0bb40663a327179a380904
alex-korneyko <slashhammer@mail.ru>
2016-03-08 18:50:57 +0200

    Add inserted exception

--- src/ThrowsExp.java
+++ src/ThrowsExp.java
@@ -1,20 +1,52 @@
+import java.io.IOException;
 
 public class ThrowsExp {
 
-    public static void main(String[] args) throws HiSpeed, DefectEngine
+    public static void main(String[] args) throws HiSpeed, DefectEngine, IOException
     {
         HiSpeed e = new HiSpeed("Very fast!");
         e.initCause(new DefectEngine("Broken carburetor"));
+        int speed = 0;
+        byte[] commandLine = new byte[20];
 
         try
         {
-            throw e;
+            try
+            {
+                System.in.read(commandLine);
+            }
+            catch (IOException ex)
+            {
+                System.out.println(ex);
+            }
+
+            for (byte symb : commandLine) {
+                if(symb != 0 & symb != 10)
+                    speed = speed * 10 + (symb - 48);
+            }
+
+            System.out.println("Current speed: " + speed);
+
+            if(speed < 10)
+            {
+                LowSpeed excp = new LowSpeed("Low speed (" + speed + ")");
+                excp.initCause(new DefectEngine("Brake carb"));
+                throw excp;
+            }
+
+            if(speed > 120)
+            {
+                HiSpeed excp = new HiSpeed("Hi speed (" + speed + ")");
+                excp.initCause(new DefectEngine("Brake carb"));
+                throw excp;
+            }
         }
-        catch (HiSpeed ex)
+        catch (HiSpeed | LowSpeed ex)
         {
             System.out.println(ex);
             System.out.println(ex.getCause());
         }
+
         finally {
             System.out.println("brake!");
         }
@@ -36,6 +68,22 @@ class HiSpeed extends Exception
     public String toString()
     {
         return _exc;
+    }
+}
+
+class LowSpeed extends Exception
+{
+    private String _message;
+
+    LowSpeed(String message)
+    {
+        _message = message;
+    }
+
+    @Override
+    public String toString()
+    {
+        return _message;
     }
 }
 
