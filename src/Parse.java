0b2ad773bf08e8164778c8d0216372d1c32dc5e8
alex-korneyko <slashhammer@mail.ru>
2016-03-08 22:33:59 +0200

    Add Parse functionality

---
+++ src/Parse.java
@@ -0,0 +1,36 @@
+/**
+ * Created by admin on 08.03.2016.
+ */
+public class Parse
+{
+    public static int ToInt(Byte[] bytes) throws ParseException
+    {
+        int result = 0;
+
+        for(byte bt: bytes)
+        {
+            try {
+                if(bt < 48 | bt > 57)
+                    throw new ParseException("Error parse");
+
+                result = result * 10 + (bt - 48);
+            }
+            catch (ParseException ex)
+            {
+                return 0;
+            }
+        }
+
+        return result;
+    }
+}
+
+class ParseException extends Exception
+{
+    private String _mess;
+
+    ParseException(String message)
+    {
+        _mess = message;
+    }
+}
